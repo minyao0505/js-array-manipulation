79c58314de27a33387e72567a31c837d5a7ef61c
minyao0505 <mlee@seekasia.com>
2023-09-26 17:02:05 +0800

    Complete kata 11 and 12

--- src/kata-11.js
+++ src/kata-11.js
@@ -2,8 +2,11 @@
  * @param {number[]} array
  * @return {number[]}
  */
-const kata11 = (array, start, quantity, ...newElems) => {
+const kata11 = (array = [], start, quantity, ...newElems) => {
   // Do something here and return an array of numbers
+  array.splice(start, quantity, ...newElems);
+
+  return array;
 };
 
 export default kata11;
--- src/kata-12.js
+++ src/kata-12.js
@@ -2,8 +2,13 @@
  * @param {number[]} array
  * @return {number}
  */
-const kata12 = (array) => {
+const kata12 = (array = []) => {
   // Do something here and return a number
+  const initialValue = 0;
+  return array.reduce(
+    (total, currentValue) => total + currentValue,
+    initialValue
+  );
 };
 
 /**
@@ -12,9 +17,12 @@ const kata12 = (array) => {
  */
 const kata12Super = (array) => {
   // Do something here and return a number
+  if (!array) return 0;
+  const initialValue = 0;
+  return array.reduce(
+    (total, currentValue) => total + currentValue.price,
+    initialValue
+  );
 };
 
-export {
-  kata12,
-  kata12Super,
-};
+export { kata12, kata12Super };
